f5124c397a6651f9e1cffa9f95475878096d0166
stuxjkee <irocks960@gmail.com>
2015-10-07 20:00:00 +0300

    8

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -4,15 +4,26 @@ class SolutionsController < ApplicationController
     @solution = Solution.new
   end
 
+  def show
+    @solution = Solution.find(params[:id])
+  end
+
   def create
     @task = Task.find_by(params[:task_id])
     @solution = @task.solutions.new(solution_params)
     @solution.user_id = current_user.id
 
-    @solution.status = "Compiling"
+    if pascal_compile(@solution.code)
+      @solution.status = "Running"
+      /TODO: run program/
+    else
+      @solution.status = "Compilation error"
+    end
+
 
     if @solution.save
-      redirect_to '/'
+
+      redirect_to @solution
     else
       redirect_to '/'
     end
@@ -21,5 +32,15 @@ class SolutionsController < ApplicationController
   private
   def solution_params
     params.require(:solution).permit(:lang, :code)
+  end
+
+  private
+  def pascal_compile(code)
+    File.open('task.pas', 'w') { |file| file.puts(code)}
+    `fpc task.pas`.include?"lines compiled"
+  end
+
+  def run
+    
   end
 end
